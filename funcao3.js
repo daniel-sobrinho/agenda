31b56b47f3acb16a9f675c58a57bee708329cd84
Daniel Sobrinho <danielcunha_sobrinho@hotmail.com>
2021-05-19 09:34:13 -0300

    upload files

--- funcao3.js
+++ funcao3.js
@@ -1,18 +1,18 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     $("input[name=ifone]").mask("00 0 0000 0000")
 })
 
-function addNewContact(){
+function addNewContact() {
     //Permitir a manipulação do Submit
     event.preventDefault();
 
     //Pegar os valores dos caompos do formulário
-    const cpNome =$("input[name='inome']").val()
+    const cpNome = $("input[name='inome']").val()
     const cpFone = $("input[name='ifone']").val()
     const cpEmail = $("input[name='iemail']").val()
     const cpLocal = $("input[name='ilocal']").val()
 
-    if(cpFone == '' && cpEmail ==''){
+    if (cpFone == '' && cpEmail == '') {
         alert('Você deve informar um telefone ou um e-mail')
         $("input[name='ifone']").focus()
         return
@@ -20,7 +20,7 @@ function addNewContact(){
 
     //criar um novo elemento
     let itemContact = $('<li></li>').text(`${cpNome} | ${cpFone} | ${cpEmail} | ${cpLocal}`)
-    
+
     //Cria botão de remover e adiciona ao item da lista
     $('<button type="button" onclick="removeContact(this)"></button>').text('Remover').appendTo(itemContact)
 
@@ -35,6 +35,6 @@ function addNewContact(){
 }
 
 //Remove o item da lista
-function removeContact(btn){
+function removeContact(btn) {
     $(btn).parent().remove()
 }
